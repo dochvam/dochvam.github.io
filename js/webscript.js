43cf4ed9aef46dead65eee359983d5d14d82ab46
Ben Goldstein <dochvam@gmail.com>
2016-12-28 16:52:45 -0500

    Slide and fade

--- js/webscript.js
+++ js/webscript.js
@@ -1,8 +1,35 @@
+var faded = false;
 
 highlight_present = function () {
 	if ($(".bluemarker").hasClass("blue")) $(".bluemarker").removeClass("blue");
 	else $(".bluemarker").addClass("blue");
 };
+
 initial_slide = function () {
-	$("right-half").addClass("giveWidth");
+	$(".right-half").addClass("new-item");
 };
+
+fade_in_title = function () {
+	$(".name").addClass("fade-text");
+	$(".subname").addClass("fade-text");
+	$(".datelink").addClass("fade-text");
+
+};
+
+
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $(".right-half").animate({
+//             width: 'toggle'
+//         });
+//     });
+// });
+
+
+sleep = function(miliseconds) {
+   var currentTime = new Date().getTime();
+
+   while (currentTime + miliseconds >= new Date().getTime()) {
+   }
+}
