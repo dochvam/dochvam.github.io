07c70a099b52ad01faeffd80ae49aab89cc55169
Ben Goldstein <dochvam@gmail.com>
2017-01-06 12:47:58 -0800

    Expanding text is go

--- js/webscript.js
+++ js/webscript.js
@@ -17,7 +17,16 @@ fade_in_title = function () {
 
 };
 
-
+$(document).ready(function()
+{
+  //hide the all of the element with class msg_body
+  $(".msg_body").hide();
+  //toggle the componenet with class msg_body
+  $(".msg_head").click(function()
+  {
+    $(this).next(".msg_body").slideToggle(600);
+  });
+});
 
 // $(document).ready(function(){
 //     $("button").click(function(){
