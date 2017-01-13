06f1ac507da0e16ae54d6c8943bfb2bd13f606b1
Ben Goldstein <dochvam@gmail.com>
2017-01-12 20:03:19 -0800

    Added subject tags.

--- js/webscript.js
+++ js/webscript.js
@@ -1,4 +1,8 @@
-var faded = false;
+ faded = false;
+ environmental_alive = false;
+ csc_alive = false;
+ philosophy_alive = false;
+ leadership_alive = false;
 
 highlight_present = function () {
 	if ($(".bluemarker").hasClass("blue")) $(".bluemarker").removeClass("blue");
@@ -7,15 +11,92 @@ highlight_present = function () {
 
 initial_slide = function () {
 	$(".right-half").addClass("new-item");
-	$(".boundary").addClass("bound-width")
+	$(".boundary").addClass("bound-width");
+  fade_in_subs();
 };
 
 fade_in_title = function () {
 	$(".name").addClass("fade-text");
-	$(".subname").addClass("fade-text");
-	$(".datelink").addClass("fade-text");
+}
 
-};
+fade_in_subs = function () {
+  $(".subname").addClass("fade-text");
+  $(".datelink").addClass("fade-text");
+  $(".category").addClass("fade-text");
+}
+
+activate_environmental = function () {
+  if (csc_alive == true) { 
+    activate_csc(); 
+  }
+  if (philosophy_alive == true) { 
+    activate_philosophy(); 
+  }
+  if (leadership_alive == true) {
+    activate_leadership();
+  }
+
+  environmental_alive = ! environmental_alive;
+
+  $(".environmental-link").toggleClass("fade-text");
+  $(".environmental").toggleClass("environmental-active");
+  $(".contains-environmental").slideToggle(600);
+}
+
+activate_csc = function () {
+  if (environmental_alive == true) { 
+    activate_environmental(); 
+  }
+  if (philosophy_alive == true) { 
+    activate_philosophy(); 
+  }
+  if (leadership_alive == true) {
+    activate_leadership();
+  }
+
+  csc_alive = !csc_alive;
+
+  $(".csc-link").toggleClass("fade-text");
+  $(".csc").toggleClass("csc-active");
+  $(".contains-csc").slideToggle(600);
+}
+
+activate_philosophy = function () {
+  if (environmental_alive == true) { 
+    activate_environmental();
+  }
+  if (csc_alive == true) { 
+    activate_csc();
+  }
+  if (leadership_alive == true) {
+    activate_leadership();
+  }
+
+  philosophy_alive = !philosophy_alive;
+
+  $(".philosophy-link").toggleClass("fade-text");
+  $(".philosophy").toggleClass("philosophy-active");
+  $(".contains-philosophy").slideToggle(600);
+}
+
+activate_leadership = function () {
+  if (environmental_alive == true) { 
+    activate_environmental();
+  }
+  if (csc_alive == true) { 
+    activate_csc();
+  }
+  if (philosophy_alive == true) { 
+    activate_philosophy(); 
+  }
+
+  leadership_alive = !leadership_alive;
+
+  $(".leadership-link").toggleClass("fade-text");
+  $(".leadership").toggleClass("leadership-active");
+  $(".contains-leadership").slideToggle(600);
+}
+
 
 $(document).ready(function()
 {
@@ -28,13 +109,6 @@ $(document).ready(function()
   });
 });
 
-// $(document).ready(function(){
-//     $("button").click(function(){
-//         $(".right-half").animate({
-//             width: 'toggle'
-//         });
-//     });
-// });
 
 
 sleep = function(miliseconds) {
